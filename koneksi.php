be44df61b244813ec5d51ded54197d1c271b18d1
Wildan F. Purnomo <37075300+wildanpurnomo@users.noreply.github.com>
2019-01-23 20:59:18 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
+$servername = "";
+$username = "";
 $password = "";
-$dbname = "kuisonline";
+$dbname = "";
 
 //Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
